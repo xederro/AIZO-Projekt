deb8425faebba005b8ab026967162e41a3632f5a
xederro <50398562+xederro@users.noreply.github.com>
2024-03-13 23:34:08 +0100

    Added cli flags for time measurements

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ build:
 	go build .\cmd\main.go
 
 run:
-	chcp 65001 & .\main.exe > data.csv
+	chcp 65001 & .\main.exe -c 100 1000 2000 4000 8000 16000 32000 64000 > data.csv
 
 plot:
 	python .\scripts\plottime.py .\data.csv plots
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "AIZO-Projekt/algo/sort"
+import (
+	"AIZO-Projekt/algo/sort"
+	"flag"
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	sort.Test(1, []int{1000, 2000, 4000, 8000, 16000, 32000, 64000})
+	tc := flag.Int("c", 0, "Count of tests")
+	flag.Parse()
+	ts := flag.Args()
+
+	if *tc != 0 {
+		if *tc < 0 {
+			panic("Count of tests must be greater than 0")
+		}
+		if ts == nil || len(ts) <= 0 {
+			panic("No test values")
+		}
+
+		args, err := parseArgs(&ts)
+		if err != nil {
+			panic("Invalid test values")
+		}
+
+		sort.Test(*tc, args)
+	} else {
+		fmt.Println("No tests to run")
+	}
+}
+
+func parseArgs(args *[]string) ([]int, error) {
+	var result []int
+	for _, arg := range *args {
+		i, err := strconv.Atoi(arg)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, i)
+	}
+	return result, nil
 }
